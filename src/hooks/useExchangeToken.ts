a12dd0acf906cd0f0da6bd85d8678ae9e1cd0842
cherrie <cherry199848@gmail.com>
2025-06-09 23:07:58 +0900

    fix: 로그인시 프로필 사진 로드 수정

--- src/hooks/useExchangeToken.ts
+++ src/hooks/useExchangeToken.ts
@@ -1,12 +1,18 @@
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { exchangeToken } from "../apis/authApi"
 import { ExchangeTokenResponse } from "../models/auth.js";
 
 const useExchangeToken = () => {
+  const queryClient = useQueryClient();
+  
   return useMutation<ExchangeTokenResponse, Error, {code: string, codeVerifier: string}>({
     mutationFn: ({code, codeVerifier}) => exchangeToken(code, codeVerifier),
     onSuccess: (data) => {
       localStorage.setItem('access_token', data.access_token);
+      //유저프로필 가져오는 훅의 쿼리 id 가져와서 캐시값 무효화(다시 실행시킴)
+      queryClient.invalidateQueries({
+        queryKey: ['current-user-profile'],
+      })
     },
   });
 };
