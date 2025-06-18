6c6384e6997de51bab37247695e8cc3fff6292ac
cherrie <cherry199848@gmail.com>
2025-06-19 01:17:53 +0900

    fix: changed alert to snackbar

--- src/layout/components/LibraryHead.tsx
+++ src/layout/components/LibraryHead.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { Box, Button, styled, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import { Box, Button, IconButton, Snackbar, SnackbarCloseReason, styled, Typography } from '@mui/material'
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import AddIcon from '@mui/icons-material/Add';
 import useCreatePlaylist from '../../hooks/useCreatePlaylist';
-import { getSpotifyAuthUrl } from '../../utils/auth';
 import useGetCurrentUserProfile from '../../hooks/useGetCurrentUserProfile';
+import CloseIcon from '@mui/icons-material/Close';
 
 //styled component
 const PlaylistHeader = styled("div")(() => ({
@@ -15,15 +15,39 @@ const PlaylistHeader = styled("div")(() => ({
 }));
 
 const LibraryHead = () => {
+  //스낵바 팝업관련
+  const [open, setOpen] = useState<boolean>(false);
+  const handleClose = (
+    event: React.SyntheticEvent | Event,
+    reason?: SnackbarCloseReason,
+  ) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+  const action = (
+    <React.Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
+
   const {data: userProfileData} = useGetCurrentUserProfile();
   const {mutate: createPlaylist} = useCreatePlaylist();
   const handleCreatePlaylist = () => {
     if(!userProfileData){
-      alert("먼저 로그인 해주세요");
-      getSpotifyAuthUrl();
+      setOpen(true);  //로그인 안했으면 로그인해주세요 스낵바 열기
     }else{
     //CreatePlaylistRequest 타입에 있는 파라미터를 보내줌
-    createPlaylist({name: "케이팝 좋아"});
+    createPlaylist({name: "My Playlist"});
     }
   }
 
@@ -36,6 +60,13 @@ const LibraryHead = () => {
       <Button onClick={handleCreatePlaylist}>
         <AddIcon sx={{ color: "#1ed760" }} />
       </Button>
+      <Snackbar
+      open={open}
+      autoHideDuration={5000}
+      onClose={handleClose}
+      message="로그인을 해주세요"
+      action={action}
+      />
     </PlaylistHeader>
   )
 }
